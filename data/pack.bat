f516ee0818a4903aeda9eab4d95b87f972cc4346
Florian Schulze <crow@icculus.org>
2002-02-17 13:31:38 +0000

    Added pack.bat to generate jumpbump.dat.

---
+++ data/pack.bat
@@ -0,0 +1,2 @@
+cd %1
+%2 -o jumpbump.dat bump.mod calib.dat death.smp fly.smp font.gob jump.mod jump.smp level.pcx levelmap.txt mask.pcx menu.pcx menumask.pcx numbers.gob objects.gob rabbit.gob scores.mod splash.smp spring.smp
